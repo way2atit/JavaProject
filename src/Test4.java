35bac427a24876945adbfb243e5e30eb260af9a6
way2atit <way2atit@gmail.com>
2023-12-08 20:38:25 +0530

    msg

--- src/Test4.java
+++ src/Test4.java
@@ -6,9 +6,11 @@ public class Test4 {
 	String name = "Admin";
 	String name1 = "Admin";
 	String name2= "Admin";
-	
-	
-	
+	int i1 =10;
+	int j2=10;
+	String name11 = "Admin";
+	String name12 = "Admin";
+	String name23= "Admin";
 	public static void main(String[] args) {
 		
 	}
