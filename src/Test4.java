077ce37d019fe3c1e4a4f1c788da36e048dd2f92
way2atit <97036773+way2atit@users.noreply.github.com>
2023-12-08 20:38:22 +0530

    Update Test4.java

--- src/Test4.java
+++ src/Test4.java
@@ -3,9 +3,12 @@ public class Test4 {
 
 	int i =10;
 	int j=10;
-	String name = "Admin";
-	String name1 = "Admin";
-	String name2= "Admin";
+	String aa = "Admin";
+	String bb = "Admin";
+	String cc= "Admin";
+	String a = "Admin";
+	String b = "Admin";
+	String c= "Admin";
 	
 	
 	
